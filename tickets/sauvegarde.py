a5981e9f2a18d8ba4cf4f16274db2ec2a96b4150
Mathieu Gosselin <math.gosselin@gmail.com>
2025-12-10 16:02:37 -0500

    modification des variables utilisées pour s'harmoniser au projet

--- tickets/sauvegarde.py
+++ tickets/sauvegarde.py
@@ -1,10 +1,14 @@
 #système de sauvegardes
 
-def sauvegarde_ticket(numero, ticket, nom, email, priorite):
-    file = open(f"{nom}.json", 'w')
-    file.write(f"Probleme:{ticket}\nNom:{nom}\nNumero:{numero}\nEmail:{email}\nPriorité:{priorite}")
+def sauvegarde_ticket(numero_ticket, probleme, nom, email):
+    file = open(f"{numero_ticket}.json", 'w')
+    file.write(f"Numero:{numero_ticket}\nProbleme:{probleme}\nNom:{nom}\nEmail:{email}\n")
     file.close()
 
-# #test
-# sauvegarde_ticket(4125, "Ca ne marche pas", "Mathieu", "math@email.com", 1)
+##-----test - doit créer un ticket appellé 4124.json dans le dossier tickets
+# numero_ticket = 4124
+# probleme = "Ca ne marche pas"
+# nom = "Mathieu"
+# email = "math@email.com"
+# sauvegarde_ticket(numero_ticket, probleme, nom, email)
 
