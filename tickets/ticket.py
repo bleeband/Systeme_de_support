90ed10fe257104fb19986c05dcafdbb17fa16f33
Marc-André Dufour <MAD@nottrue.test>
2025-12-15 17:25:44 -0500

    fix:comment test

--- tickets/ticket.py
+++ tickets/ticket.py
@@ -31,5 +31,6 @@ def collecter_info_ticket():
     print("=== TERMINER ===")
 
     return prenom, nom, courriel, description, priorite
-    
-collecter_info_ticket()
+
+# TEST
+# collecter_info_ticket()
