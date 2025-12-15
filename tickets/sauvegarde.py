bc31e84f6bdd62292260f18c37d070c15003479a
Mathieu Gosselin <math.gosselin@gmail.com>
2025-12-15 16:27:05 -0500

    doc: ajout de commentaires pour sauvegarde.py

--- tickets/sauvegarde.py
+++ tickets/sauvegarde.py
@@ -1,11 +1,14 @@
-#système de sauvegardes
+#système de sauvegarde
 
 def sauvegarde_ticket(numero_ticket, probleme, nom, email):
+    # on ouvre un fichier, si inexistant ça le crée au format numero_de_ticket.json
     file = open(f"{numero_ticket}.json", 'w')
+    # on inscrit dans l'info dans le .json
     file.write(f"Numero:{numero_ticket}\nProbleme:{probleme}\nNom:{nom}\nEmail:{email}\n")
+    # on ferme le fichier
     file.close()
 
-##-----test - doit créer un ticket appellé 4124.json dans le dossier tickets
+# #-----test - doit créer un ticket appellé 4124.json dans le dossier tickets
 # numero_ticket = 4124
 # probleme = "Ca ne marche pas"
 # nom = "Mathieu"
